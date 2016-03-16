c7c49cd2fb294b9e7592b6879832d65527edf5cd
Aashni Shah <contact@aashni.me>
2016-03-16 04:07:08 -0400

    multiple lines of html div tags

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+
+// one way to display content
+// class component - can have a state
 class App extends React.Component {
   render(){
-    return <div>Hello World</div>
+  	// using HTML/JSX: 
+    return (
+    	<div>
+    		<h1>Hello World</h1>
+    		<b>This is me</b>
+    	</div>
+    );
+    // using React native
+    // return React.createElement('h1', null, 'Hello World')
   }
 }
+
+// stateless component with no state
+// const App = () => <h1>Hello World</h1>
 
 export default App

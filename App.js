4957d58bb1223bbe549dc9c3cee0bd799365a323
Aashni Shah <contact@aashni.me>
2016-03-21 02:29:52 -0400

    creating a widget for reusable code

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
 		this.state = {
 			txt: 'this is some text'
 		}
+		this.update = this.update.bind(this)
 	}
 	update(e) {
 		this.setState({
@@ -16,11 +17,22 @@ class App extends React.Component {
 	render(){
 		return (
 			<div>
-				<input type="txt" onChange={this.update.bind(this)} />
-				<h1>{this.state.txt}</h1>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		);
 	}
+}
+
+const Widget = (props) => {
+	return (
+		<div>
+			<input type="txt" onChange={props.update} />
+			<h1>{props.txt}</h1>
+		</div>
+	);
 }
 
 export default App

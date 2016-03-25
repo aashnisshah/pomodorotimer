3e6081092779501200b66595b14da8a31bb952a6
Aashni Shah <contact@aashni.me>
2016-03-25 04:37:40 -0400

    got  timer to countdown, and to stop at 0 seconds

--- App.js
+++ App.js
@@ -5,41 +5,56 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			input: '/* add your jsx here */',
-			output: '',
-			err: ''
+			val: 3
 		}
 		this.update = this.update.bind(this);
 	}
-	update(e) {
-		let code = e.target.value;
-		try {
-			this.setState({
-				output: babel.transform(code, {
-					stage: 0,
-					loose: 'all',
-				}).code
-			})
-		} catch(err) {
-			this.setState({
-				err: err.message
-			})
+	update() {
+		this.setState({val: this.state.val - 1})
+		if (this.state.val <= 0) {
+			clearInterval(this.inc)
+			console.log('timer done')
 		}
+	}
+	componentWillMount() {
+		// when component is fully prepped and ready to enter the DOM
+		// this.setState({m: 2})
+	}
+	render() {
+		console.log('rendering')
+		return <button onClick={this.update}>{this.state.val}</button>
+	}
+	componentDidMount() {
+		// after component exists in the DOM
+		console.log(ReactDOM.findDOMNode(this))
+		// calls this.update every 0.5 seconds
+		this.inc = setInterval(this.update, 1000)
+	}
+	componentWillUnmount() {
+		console.log('bye!')
+		clearInterval(this.inc)
+	}
+}
+
+class Wrapper extends React.Component {
+	constructor() {
+		super();
+	}
+	mount() {
+		ReactDOM.render(<App />, document.getElementById('a'))
+	}
+	unmount() {
+		ReactDOM.unmountComponentAtNode(document.getElementById('a'))
 	}
 	render() {
 		return (
 			<div>
-				<header>{this.state.err}</header>
-				<div className="container">
-					<textarea
-						onChange={this.update}
-						defaultValue={this.state.input}>
-					</textarea>
-					<pre>{this.update.output}</pre>
-				</div>
+				<button onClick={this.mount.bind(this)}>Start</button>
+				<button onClick={this.unmount.bind(this)}>Reset</button>
+				<div id="a"></div>
 			</div>
 		)
 	}
 }
 
-export default App
+export default Wrapper

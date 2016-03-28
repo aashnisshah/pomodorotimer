add65125741d3554aec482389c2109f7b561fe7b
Aashni Shah <contact@aashni.me>
2016-03-28 05:10:15 -0400

    adding input box to get time

--- App.js
+++ App.js
@@ -70,9 +70,21 @@ class Wrapper extends React.Component {
 		ReactDOM.unmountComponentAtNode(document.getElementById('countdown'))
 		ReactDOM.render(<App />, document.getElementById('countdown'))
 	}
+	update(e) {
+		//  hook up to global variable for time
+	}
 	render() {
 		return (
 			<div>
+				<NumInput
+						ref="red"
+						min={0}
+						max={255}
+						step={1}
+						val={5}
+						type="number" // swap between slider or input field
+						label="minutes"
+						update={this.update} />
 				<div className="btn-group">
 					<button
 						onClick={this.mount.bind(this)}
@@ -100,6 +112,44 @@ class Wrapper extends React.Component {
 			</div>
 		)
 	}
+}
+
+class NumInput extends React.Component {
+	render() {
+		let label = this.props.label !== '' ?
+			<label>{this.props.label} - {this.props.val}</label> : ''
+		return (
+			<div>
+				<input ref="inp"
+					type={this.props.type}
+					min={this.props.min}
+					max={this.props.max}
+					step={this.props.step}
+					defaultValue={this.props.val}
+					onChange={this.props.update} />
+				{label}
+			</div>
+		)
+	}
+}
+
+NumInput.propTypes = {
+	min: React.PropTypes.number,
+	max: React.PropTypes.number,
+	step: React.PropTypes.number,
+	val: React.PropTypes.number,
+	label: React.PropTypes.string,
+	update: React.PropTypes.func.isRequired,
+	type: React.PropTypes.oneOf(['number','range'])
+}
+
+NumInput.defaultProps = {
+	min: 0,
+	max: 0,
+	step: 1,
+	val: 0,
+	label: '',
+	type: 'range'
 }
 
 export default Wrapper
